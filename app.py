3cc40eab0ee9f1b708a06473ea9441590a987bab
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-24 17:11:50 -0700

    docstrings

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def generate_template():
+    """Generates a template of story prompts for user to fill in"""
     words = silly_story.prompts
 
     return render_template('questions.jinja', word_prompt=words)
@@ -18,7 +19,8 @@ def generate_template():
 
 @app.get('/results')
 def get_answers():
-    ans = request.args
+    """Gets answers provided by users in story prompt and updates results.jinja"""
+    ans = request.args  # this is not exactly a dictionary but can be treated like a dictionary
     results = silly_story.get_result_text(ans)
 
     return render_template('results.jinja', story=results)
